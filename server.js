8d8d7fcbc1546426d0c22acfa85846900b97b3d0
unknown <subhamsbharadwaz@gmail.com>
2021-03-22 21:40:43 +0530

    csrf bugg

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const dotEnv = require('dotenv');
-const morgan = require('morgan');
-const colors = require('colors');
-const cookieParser = require('cookie-parser');
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const connectDB = require('./config/db');
-const errorHandler = require('./middleware/error');
-const csurf = require('csurf');
+const express = require("express");
+const dotEnv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
 
 // Load ENV vars
-dotEnv.config({ path: './config/config.env' });
+dotEnv.config({ path: "./config/config.env" });
 
 const app = express();
 
@@ -23,14 +22,12 @@ app.use(express.json());
 
 // Cookie Parser
 app.use(cookieParser());
-// CSURF
-app.use(csurf());
 
 const PORT = process.env.PORT || 5000;
 
 // Dev logging middleware
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Set security headers
@@ -43,9 +40,9 @@ app.use(xss());
 app.use(mongoSanitize());
 
 // Mount Router
-app.use('/api/v1/expenses', require('./routes/expense'));
-app.use('/api/v1/todos', require('./routes/todos'));
-app.use('/api/v1/auth', require('./routes/auth'));
+app.use("/api/v1/expenses", require("./routes/expense"));
+app.use("/api/v1/todos", require("./routes/todos"));
+app.use("/api/v1/auth", require("./routes/auth"));
 
 app.use(errorHandler);
 
@@ -58,7 +55,7 @@ const server = app.listen(
 
 // Handled unhandled rejections
 
-process.on('unhandledRejection', (err, promise) => {
+process.on("unhandledRejection", (err, promise) => {
   console.log(`Error: ${err.message}`.red);
   // Close server & exit process
   server.close(() => process.exit(1));
