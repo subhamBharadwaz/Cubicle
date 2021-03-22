0b184b9713d7f57a3679c6c9234658a6398bcded
unknown <subhamsbharadwaz@gmail.com>
2021-03-22 17:47:12 +0530

    minor changes

--- client/src/api/weatherApi.js
+++ client/src/api/weatherApi.js
@@ -1,6 +1,7 @@
 //  to prevent the cors error for sometime use "https://cors-anywhere.herokuapp.com/" OR
 //  https://cors.bridged.cc/
-const base_url = 'https://cors.bridged.cc/http://api.openweathermap.org/';
+const base_url =
+  'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/';
 const appid = `&appid=${process.env.REACT_APP_WEATHER_API}`;
 
 export const byCityName = (city) =>
