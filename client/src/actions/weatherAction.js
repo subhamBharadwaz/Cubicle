ec15d8a405892d8e14703d6629ecf8d4fb7de689
Rahul Hazarika <rcm.abhyan5@gmail.com>
2021-09-14 19:28:00 +0530

    :art: style(responsive styles)

--- client/src/actions/weatherAction.js
+++ client/src/actions/weatherAction.js
@@ -3,17 +3,29 @@ import { byCityName } from "../api/weatherApi";
 
 export const fetchWeather = (city) => async (dispatch) => {
   const config = {
+    // headers: {
+    //   "Access-Control-Allow-Origin": "*",
+    //   "X-Requested-With": "XMLHttpRequest",
+    // },
+    headers: {
+      "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+      "x-rapidapi-key": "9588618ab9msh95a5fecfc233ed7p1274e1jsn39de38302645",
+    },
+  };
+  // const weatherData = await axios.get(byCityName(city), config);
+  const weather = await fetch(byCityName(city), {
+    method: "GET",
+    mode: "cors",
     headers: {
       "Access-Control-Allow-Origin": "*",
       "X-Requested-With": "XMLHttpRequest",
     },
-  };
-  const weatherData = await axios.get(byCityName(city), config);
-
+  });
+  const weatherData = await weather.json();
   dispatch({
     type: "FETCH_DATA",
     payload: {
-      weatherInfo: weatherData.data,
+      weatherInfo: weatherData,
     },
   });
 };
--- client/src/api/weatherApi.js
+++ client/src/api/weatherApi.js
@@ -1,7 +1,8 @@
 //  to prevent the cors error for sometime use "https://cors-anywhere.herokuapp.com/" OR
 //  https://cors.bridged.cc/
 const base_url =
-  "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/";
+  // "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/";
+  "https://cors.bridged.cc/http://api.openweathermap.org/";
 const appid = `&appid=${process.env.REACT_APP_WEATHER_API}`;
 
 export const byCityName = (city) =>
